e971f167d2a19f3ae6e9f68999a24df6d2847211
Gabriel de Azevedo <184437223+gabriel-de-azevedo@users.noreply.github.com>
2025-10-04 08:52:23 -0300

    feat: primitive_types

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,6 +11,12 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(amount: u32) -> u32 {
+    if amount > 40 {
+        return amount;
+    }
+    amount * 2
+}
 
 fn main() {
     // You can optionally experiment here.
